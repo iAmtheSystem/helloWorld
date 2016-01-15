b86f347009dbc25ef117ffc6b90c2cd1ae59b6eb
iAmTheSystem <simonreichel@yahoo.de>
2016-01-15 01:43:57 +0100

    Added HelloWorld Code and git Version Controll

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Simon Reichel
+// Version     :
+// Copyright   : None
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
